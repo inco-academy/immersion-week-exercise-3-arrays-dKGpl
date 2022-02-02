047af8cb884c42ed7665ae209594b8c24400c614
darekgrabowski <dariuszgrabowski@mixbudkg.pl>
2022-02-02 18:28:48 +0100

    Fix to test files

--- src/3_average_min_max.test.js
+++ src/3_average_min_max.test.js
@@ -1,13 +1,32 @@
 const averageMinMax = require("./3_average_min_max");
 
-test("Average Min Max (1)", () => {
-  expect(averageMinMax([-42, 0, 42])).toBe("Min: -42 Max: 42 Average: 0");
+let consoleSpy;
+
+beforeEach(() => {
+  consoleSpy = jest.spyOn(console, "log");
 });
 
-test("Average Min Max (2)", () => {
-  expect(averageMinMax([30, 20, 100])).toBe("Min: 20 Max: 100 Average: 50");
+afterEach(() => {
+  consoleSpy.mockClear();
 });
 
-test("Average Min Max (3)", () => {
-  expect(averageMinMax([-23, -4, -12])).toBe("Min: -23 Max: -4 Average: -13");
+test("1", () => {
+  averageMinMax([-42, 0, 42]);
+
+  expect(consoleSpy).toHaveBeenCalledTimes(1);
+  expect(consoleSpy).toHaveBeenCalledWith("Min: -42 Max: 42 Average: 0");
+});
+
+test("2", () => {
+  averageMinMax([30, 20, 100]);
+
+  expect(consoleSpy).toHaveBeenCalledTimes(1);
+  expect(consoleSpy).toHaveBeenCalledWith("Min: 20 Max: 100 Average: 50");
+});
+
+test("3", () => {
+  averageMinMax([-23, -4, -12]);
+
+  expect(consoleSpy).toHaveBeenCalledTimes(1);
+  expect(consoleSpy).toHaveBeenCalledWith("Min: -23 Max: -4 Average: -13");
 });
